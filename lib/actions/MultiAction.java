c4c8a299d791efec14f8aa04d281676dd0301622
Cabey6 <b3nschwarz@gmail.com>
2019-09-03 17:54:11 +1000

    Added Parrallel MutliAction

--- lib/actions/MultiAction.java
+++ lib/actions/MultiAction.java
@@ -13,22 +13,28 @@ import spiderling.lib.logic.LogicOperators;
  */
 public class MultiAction {
 
-
-    public static class Sequential{
+    public static class Sequential extends Action{
         Check check = null;
 
 
         int counter = 0;
         Action[] actions;
 
+
+        public Sequential(Check check) {
+            super(check);
+        }
         /**
          * Constructor to run multiple actions consecutively
          * Will only finish after all actions in the sequence are complete.
          * @param actions The sequence of actions to be completed.
          */
         public Sequential(Action... actions) {
+            super(new ChTrue()); //FIXME Temp change to remove errors.
+            new ChGettableBoolean(() -> counter >= actions.length, true);
             this.actions = actions;
-            this.check = new ChGettableBoolean(() -> counter >= actions.length, true);
+
+
         }
         /**
          * Constructor to run multiple actions consecutively
@@ -37,6 +43,7 @@ public class MultiAction {
          * @param actions The sequence of actions to be completed.
          */
         public Sequential (Check check, boolean mustFinishSequence, Action... actions) {
+            super((new ChTrue())); //FIXME Temp changes to remove errors.
             if (mustFinishSequence) this.check = new ChMulti(LogicOperators.AND, new ChGettableBoolean(() -> counter >= actions.length, true), check);
             else this.check = new ChMulti(LogicOperators.OR, new ChGettableBoolean(() -> counter >= actions.length, true), check);
         }
@@ -67,7 +74,70 @@ public class MultiAction {
 
     }
 
-    public static class Parallel{
+    public static class Parallel extends Action{
+        Check check = null;
 
-    }
+
+        int counter = 0;
+        Action[] actions;
+
+
+        /**
+         * Constructor to run multiple actions consecutively
+         * Will only finish after all actions in the sequence are complete.
+         * @param actions The sequence of actions to be completed.
+         */
+        public Parallel(Action... actions) {
+            super(new ChTrue()); //FIXME Temp change to remove errors.
+            new ChGettableBoolean(() -> counter >= actions.length, true);
+            this.actions = actions;
+
+
+        }
+        /**
+         * Constructor to run multiple actions consecutively
+         * @param mustFinishSequence Determines if the action will conclude if either the inputted check is completed or the sequence is complete, or if both the check and the sequence need to complete.
+         * @param check The check to determine
+         * @param actions The sequence of actions to be completed.
+         */
+        public Parallel (Check check, boolean mustFinishSequence, Action... actions) {
+            super((new ChTrue())); //FIXME Temp changes to remove errors.
+            if (mustFinishSequence) this.check = new ChMulti(LogicOperators.AND, new ChGettableBoolean(() -> counter >= actions.length, true), check);
+            else this.check = new ChMulti(LogicOperators.OR, new ChGettableBoolean(() -> counter >= actions.length, true), check);
+        }
+
+        /**
+         * Used by individual actions to set other conditions for the action to be complete.
+         *
+         * @return Whether the action should stop running.
+         */
+        protected boolean isDone() {
+            return false;
+        }
+
+        /**
+         * Calls the action loop for each action that hasn't yet concluded.
+         * It calls parrallelActionLoop instead of Action.actionLoop which instead
+         */
+        protected void onRun() {
+            for(Action action: actions) {
+                if(counter < actions.length) {
+                    if (action.isFinished())
+                    if (parrallelActionLoop(actions[counter])) {
+                        counter++;
+                    }
+                }
+            }
+        }
+        public final boolean parrallelActionLoop(Action action) {
+            if (!action.isRunning) action.initialise();
+            if (!action.isDone()) action.execute();
+            if (action.isDone()) {
+                action.end();
+                return true;
+            }
+            return false;
+        }
 }
+}
+
