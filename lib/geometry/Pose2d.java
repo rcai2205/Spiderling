f434706227c359904c5236ab200136a0e5944fa7
Austin Lin <21chydra@gmail.com>
2020-07-28 20:47:59 +1000

    Integrated mecanum kinematics from wpilib

--- lib/geometry/Pose2d.java
+++ lib/geometry/Pose2d.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package edu.wpi.first.wpilibj.geometry;
+package spiderling.lib.geometry;
 
 import java.util.Objects;
 
